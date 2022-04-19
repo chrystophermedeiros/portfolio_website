61fee4319f95a90d65c3cd285ab9df49a3c5f1b5
chrystophermedeiros <chrystopher312@gmail.com>
2022-04-18 21:16:26 -0300

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./nav.css"
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const {activeNav, setAxtiveNav} = useState("#")
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className={activeNav === '#' ? 'acitve' :''}><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+
+      
+    </nav>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,7 @@ import "./services.css"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>Serviços</section>
   )
 }
 
