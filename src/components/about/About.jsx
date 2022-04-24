cd31a0ed33c7356334548812a3fe6bba9af7c915
chrystophermedeiros <chrystopher312@gmail.com>
2022-04-24 14:08:11 -0300

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/hacker.png'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
 
-
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h5>Conhecer</h5>
       <h2>Sobre mim</h2>
       <div className="container about_container">
@@ -16,34 +15,38 @@ const About = () => {
           <div className="about_me-image">
             <img src={ME} alt="Minha imagem" />
           </div>
-
         </div>
 
         <div className="about_content">
-
           <div className="about_cards">
-
-
-            <article className='about_card'><FaAward className='about_icon' />
+            <article className="about_card">
+              <FaAward className="about_icon" />
               <h5>Experiencia</h5>
               <small>Mais de 3 anos de trabalho </small>
             </article>
 
-            <article className='about_card'><FiUsers className='about_icon' />
+            <article className="about_card">
+              <FiUsers className="about_icon" />
               <h5>Clientes</h5>
               <small>Atualmente 2 clientes no Brasil </small>
             </article>
 
-            <article className='about_card'><VscFolderLibrary className='about_icon' />
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
               <h5>Projetos</h5>
               <small>5 projetos completos </small>
             </article>
           </div>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloribus quam inventore asperiores, autem quaerat tempora voluptas ut doloremque perspiciatis dicta numquam quas iste repellendus voluptatem eaque. Fugit, eaque explicabo.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            doloribus quam inventore asperiores, autem quaerat tempora voluptas
+            ut doloremque perspiciatis dicta numquam quas iste repellendus
+            voluptatem eaque. Fugit, eaque explicabo.
           </p>
 
-          <a href="#contact" className='btn btn-primary'>Vamos conversar</a>
+          <a href="#contact" className="btn btn-primary">
+            Vamos conversar
+          </a>
         </div>
       </div>
     </section>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/hackerr.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
