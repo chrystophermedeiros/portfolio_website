a6bc0d3ea3d20f3e3c159e72a6c5760de5c00e51
chrystophermedeiros <91420438+chrystophermedeiros@users.noreply.github.com>
2022-05-15 16:02:37 -0300

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,18 +8,22 @@ import {BiMessageSquareDetail} from 'react-icons/bi'
 import {useState} from 'react'
 
 const Nav = () => {
-  const {activeNav, setAxtiveNav} = useState("#")
-  return (
-    <nav>
-      <a href="#" onClick={() => setAxtiveNav('#')}  className={activeNav === '#' ? 'acitve' :''}><AiOutlineHome/></a>
-      <a href="#about" onClick={() => setAxtiveNav('#about')} className={activeNav === '#about' ? 'active' :''}><AiOutlineUser/></a>
-      <a href="#experience" onClick={() => setAxtiveNav('#experience')} className={activeNav === '#experience' ? 'active' :''}><BiBook/></a>
-      <a href="#services" onClick={() => setAxtiveNav('#services')} className={activeNav === '#services' ? 'active' :''}><RiServiceLine/></a>
-      <a href="#contact" onClick={() => setAxtiveNav('#contact')} className={activeNav === '#contact' ? 'active' :''}><BiMessageSquareDetail/></a>
+  const {activeNav, setActiveNav} = useState("#")
+    return (
+        <nav>
+          <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
 
-      
-    </nav>
-  )
+          <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+
+          <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+
+          <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+
+          <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+
+          
+        </nav>
+    )
 }
 
 export default Nav
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,65 +5,81 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+const data =[
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut obcaecati repudiandae provident fugit labore tempore repellat incidunt, error voluptas sit quae modi et laudantium rem quos eum soluta delectus.'
+  },
+
+  {
+    avatar: AVTR2,
+    name: 'Shatta wale',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut obcaecati repudiandae provident fugit labore tempore repellat incidunt, error voluptas sit quae modi et laudantium rem quos eum soluta delectus.'
+  },
+
+
+  {
+    avatar: AVTR3,
+    name: 'KwameDespite',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut obcaecati repudiandae provident fugit labore tempore repellat incidunt, error voluptas sit quae modi et laudantium rem quos eum soluta delectus.'
+  },
+
+
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ut obcaecati repudiandae provident fugit labore tempore repellat incidunt, error voluptas sit quae modi et laudantium rem quos eum soluta delectus.'
+  },
+
+
+
+]
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
       <h5>Relatos dos clientes</h5>
       <h2>Testemunhas</h2>
-      <div className='container testimonials_container'>
+      <Swiper className='container testimonials_container'
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) =>{
+              return (
+                <SwiperSlide key= {index} className='testimonial'>
+                <div className="client_avatar">
+                  <img src={avatar}/>
+                </div>
+                <div>
+                <h5 className='client_name'>{name}</h5>
+                  <small className='client_review'>
+                    {review}
+                  </small>
+                </div>
+              </SwiperSlide>
+              )
+          })
         
-        <article className='testimonial'>
-          <div className="client_avatar">
-            <img src={AVTR1} alt="avatar 1" />
-          </div>
-          <div>
-          <h5 className='client_name'>Ernestro Sobreira</h5>
-            <small className='client_review'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </small>
-          </div>
-        </article>
-       
-
-        <article className='testimonial'>
-          <div className="client_avatar">
-            <img src={AVTR2} alt="avatar 2" />
-          </div>
-          <div>
-          <h5 className='client_name'>Ernestro Sobreira</h5>
-            <small className='client_review'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </small>
-          </div>
-        </article>
-
-        <article className='testimonial'>
-          <div className="client_avatar">
-            <img src={AVTR3} alt="avatar 3" />
-          </div>
-          <div>
-          <h5 className='client_name'>Ernestro Sobreira</h5>
-            <small className='client_review'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </small>
-          </div>
-        </article>
-
-        <article className='testimonial'>
-          <div className="client_avatar">
-            <img src={AVTR4} alt="avatar 4" />
-          </div>
-          <div>
-          <h5 className='client_name'>Ernestro Sobreira</h5>
-            <small className='client_review'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </small>
-          </div>
-        </article>
-
-        
+        }
+           
     
-      </div>
+      </Swiper>
     </section>
   )
 }
