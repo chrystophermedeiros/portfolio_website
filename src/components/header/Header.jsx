3f1fedd7ed3126f8985b29593b76c8a98daa602a
chrystophermedeiros <91420438+chrystophermedeiros@users.noreply.github.com>
2022-08-26 10:17:35 -0300

    Update me em Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from './CTA'
-import ME from '../../assets/hackerr.png'
+import ME from '../../assets/chrystopher2.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
