166edb9d302e4b71a7f5dc13db4cd7defef0d837
chrystophermedeiros <chrystopher312@gmail.com>
2023-07-08 11:48:55 -0300

    update imagem

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./header.css"
 import CTA from './CTA'
-import ME from '../../assets/chrystopher2.png'
+import ME from '../../assets/eu26.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
